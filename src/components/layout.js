eff9d3947bd7b0c6c39399942fa0174f8387305a
Cinthia <cinthia.j.23@gmail.com>
2020-05-17 21:21:14 +1000

    gatsby configuration

---
+++ src/components/layout.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { css } from "@emotion/core"
+import { useStaticQuery, Link, graphql } from "gatsby"
+import { rhythm } from "../utils/typography"
+
+export default function Layout({ children }) {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
+  return (
+    <div
+      css={css`
+        margin: 0 auto;
+        max-width: 700px;
+        padding: ${rhythm(2)};
+        padding-top: ${rhythm(1.5)};
+      `}
+    >
+      <Link to={`/`}>{data.site.siteMetadata.title}</Link>
+      <Link to={`/blog/`}>Blog</Link>
+      <Link to={`/portfolio/`}>Portfolio</Link>
+      {children}
+    </div>
+  )
+}
---
+++ src/pages/portfolio.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function Portfolio() {
+  return (
+    <Layout>
+      <h1>About Pandas Eating Lots</h1>
+      <p>PROJECTSSSSSSSSSSSSSSSSS!!!!!!!!!!!!!</p>
+    </Layout>
+  )
+}
